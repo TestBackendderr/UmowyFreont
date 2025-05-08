60e5f38388b6f3dcfe163b78facf508c8b68558e
TestBackendderr <olegmat2002@gmail.com>
2025-05-08 20:44:40 +0200

    fix

--- components/layouts/MainLayout.jsx
+++ components/layouts/MainLayout.jsx
@@ -4,13 +4,13 @@ import Leftside from "../Leftside";
 import Middleside from "../Middleside";
 import Rightside from "../Rightside";
 
-export default function MainLayout() {
+export default function MainLayout({ children }) {
   return (
     <div className="main-layout">
       <Navbar />
       <div className="content">
         <Leftside />
-        <Middleside />
+        <main>{children}</main>
         <Rightside />
       </div>
     </div>
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,6 +1,11 @@
 import MainLayout from "@/components/layouts/MainLayout";
+import Middleside from "@/components/Middleside";
 import React from "react";
 
 export default function Home() {
-  return <MainLayout />;
+  return (
+    <MainLayout>
+      <Middleside />
+    </MainLayout>
+  );
 }
