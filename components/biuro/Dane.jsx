09d4a0139d42be7c266fac1ed082107e20e247d4
TestBackendderr <olegmat2002@gmail.com>
2025-05-16 18:53:05 +0200

    fix address

--- components/biuro/Dane.jsx
+++ components/biuro/Dane.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 
 const Dane = ({ umowa }) => {
+  let adres = {
+    ulica: "",
+    miejscowosc: "",
+    kodPocztowy: "",
+    powiat: "",
+    wojewodztwo: "",
+  };
+
+  switch (umowa.miejsceInstalacji) {
+    case "Adres klienta":
+      adres = {
+        ulica: umowa.ulica,
+        miejscowosc: umowa.miejscowosc,
+        kodPocztowy: umowa.kodPocztowy,
+        powiat: umowa.powiat,
+        wojewodztwo: umowa.wojewodztwo,
+      };
+      break;
+    case "Adres korespondencyjny":
+      adres = {
+        ulica: umowa.adresUlica,
+        miejscowosc: umowa.adresMiejscowosc,
+        kodPocztowy: umowa.adresKodPocztowy,
+        powiat: umowa.adresPowiat,
+        wojewodztwo: umowa.adresWojewodztwo,
+      };
+      break;
+    case "Inny":
+      adres = {
+        ulica: umowa.miUlica,
+        miejscowosc: umowa.miMiejscowosc,
+        kodPocztowy: umowa.miKod,
+        powiat: umowa.miPowiat,
+        wojewodztwo: umowa.miWojewodztwo,
+      };
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="biuro2-section">
-      <h3>Dane</h3>
-      <p>{umowa.imieNazwisko}</p>
-      <p>Ul. Główna 1/1</p>
-      <p>20-200 Mięścowośc</p>
-      <p>Woj. mazowieckie</p>
-      <p>{umowa.telefon}</p>
-      <p>{umowa.email}</p>
+      <h3>Adres instalacji</h3>
+      <p>Ul. {adres.ulica}</p>
+      <p>
+        {adres.kodPocztowy} {adres.miejscowosc}
+      </p>
+      <p>Powiat: {adres.powiat}</p>
+      <p>Województwo: {adres.wojewodztwo}</p>
     </div>
   );
 };
